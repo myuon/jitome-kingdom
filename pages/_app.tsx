e2fdd34cc920375815c5f93d6f1a5d9a2a1ca61d
myuon <ioi.joi.koi.loi@gmail.com>
2020-03-15 20:04:44 +0900

    feat: index page

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import "ress";
+import React from "react";
+import { Global, css } from "@emotion/core";
+import { ThemeProvider } from "emotion-theming";
+import { Theme } from "../components/Theme";
+
+const theme: Theme = {
+  palette: {
+    primary: {
+      base: "#e61865",
+      light: "#fedae0",
+      dark: "#b91150"
+    }
+  }
+};
+
+const MyApp: React.FC<{ Component: any; pageProps: any }> = ({
+  Component,
+  pageProps
+}) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Global
+        styles={css`
+          main {
+            max-width: 1280px;
+            margin: auto;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Typography, Toolbar, Button, AppBar } from "@material-ui/core";
+import { css } from "@emotion/core";
 
 const Index: React.FC = props => {
-  return <h1>hello!</h1>;
+  return (
+    <React.Fragment>
+      <AppBar position="static" elevation={0}>
+        <Toolbar>
+          <div
+            css={css`
+              flex-grow: 1;
+            `}
+          >
+            <Typography variant="h6">ジト目王国</Typography>
+          </div>
+          <Button color="inherit">ログイン</Button>
+        </Toolbar>
+      </AppBar>
+
+      <main>
+        <p>index</p>
+        <div
+          css={css`
+            display: flex;
+            justify-content: center;
+          `}
+        >
+          <Button variant="contained" color="primary">
+            ログインしてスタート
+          </Button>
+        </div>
+      </main>
+
+      <footer>ふったー</footer>
+    </React.Fragment>
+  );
 };
 
 export default Index;
