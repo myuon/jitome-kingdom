987dcfa60f3d67d09a802f79d96664b9bd9c5639
myuon <ioi.joi.koi.loi@gmail.com>
2020-03-23 01:57:04 +0900

    fix: meta tags in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { Global, css } from "@emotion/core";
 import { ThemeProvider } from "emotion-theming";
 import { Theme } from "../src/components/Theme";
 import { useAuth, AuthProvider } from "../src/hooks/useAuth";
+import Head from "next/head";
 
 const theme: Theme = {
   palette: {
@@ -33,6 +34,22 @@ const MyApp: React.FC<{ Component: any; pageProps: any }> = ({
             }
           `}
         />
+        <Head>
+          <title>Jitome Kingdom</title>
+          <meta property="og:title" content="ジト目王国" />
+          <meta
+            property="og:description"
+            content="Welcome to Jitome Kingdom!"
+          />
+          <meta property="og:url" content="https://jitome.ramda.io" />
+          <meta
+            property="og:image"
+            content="https://jitome.ramda.io/image/top_jitome.png"
+          />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@myuon_myon" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
         <Component {...pageProps} />
       </AuthProvider>
     </ThemeProvider>
